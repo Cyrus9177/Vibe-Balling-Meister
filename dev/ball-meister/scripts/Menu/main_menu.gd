d49f03b077fbaedcc83e595c67b804188e1fa718
Kenosis0 <johnromelflores00@gmail.com>
2026-05-01 16:17:02 +0800

    Add main menu, assets, and UI theme - Flores
    Introduce a new main menu scene and related UI/asset resources: adds menu scene (main_menu.tscn), menu script with button handlers (main_menu.gd) and quit action, a themed button resource (resources/menu_button.tres), background texture and menu image, and two font files with import metadata. Also add a simple @tool Path2D script (ball_paths.gd) for level paths and a project display/stretch setting in project.godot. UID/import metadata files were added for Godot resource tracking.

---
+++ dev/ball-meister/scripts/Menu/main_menu.gd
@@ -0,0 +1,17 @@
+extends CanvasLayer
+
+
+func _on_continue_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_play_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_level_selection_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
---
+++ dev/ball-meister/scripts/ball_paths.gd
@@ -0,0 +1,2 @@
+@tool
+extends Path2D
